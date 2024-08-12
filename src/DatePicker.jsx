a3038404e44c0780f70f39acc912f82611215715
yator <cherutichfelix33@gmail.com>
2024-08-12 16:55:36 +0300

    commit

--- src/DatePicker.jsx
+++ src/DatePicker.jsx
@@ -18,6 +18,8 @@ export function DatePicker({ value, onChange }){
     )
 }
 
+
+
 function DatePickerModel({value, onChange}){
   const[visibleMonth, setVisibleMonth] = useState(value || new Date())
 
@@ -69,41 +71,6 @@ function DatePickerModel({value, onChange}){
               {date.getDate()}
             </button>
           ))}
-          {/* <button className="date date-picker-other-month-date">28</button>
-          <button className="date date-picker-other-month-date">29</button>
-          <button className="date date-picker-other-month-date">30</button>
-          <button className="date date-picker-other-month-date">31</button>
-          <button className="date">1</button>
-          <button className="date">2</button>
-          <button className="date">3</button>
-          <button className="date">4</button>
-          <button className="date">5</button>
-          <button className="date">6</button>
-          <button className="date">7</button>
-          <button className="date">8</button>
-          <button className="date">9</button>
-          <button className="date">10</button>
-          <button className="date">11</button>
-          <button className="date">12</button>
-          <button className="date">13</button>
-          <button className="date">14</button>
-          <button className="date">15</button>
-          <button className="date">16</button>
-          <button className="date">17</button>
-          <button className="date">18</button>
-          <button className="date">19</button>
-          <button className="date">20</button>
-          <button className="date">21</button>
-          <button className="date">22</button>
-          <button className="date">23</button>
-          <button className="date">24</button>
-          <button className="date">25</button>
-          <button className="date selected">26</button>
-          <button className="date">27</button>
-          <button className="date">28</button>
-          <button className="date today">29</button>
-          <button className="date">30</button>
-          <button className="date date-picker-other-month-date">1</button> */}
         </div>
       </div>
   )
